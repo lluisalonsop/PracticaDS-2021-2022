68a6d81304e6b049840588a85da5a365ae435da8
Biel Camprubí <bielcamprubi@gmail.com>
2021-10-20 19:52:52 +0200

    Tasks updated

--- src/Node.java
+++ src/Node.java
@@ -1,48 +1,52 @@
 import java.util.LinkedList;
 import java.util.List;
 
-
 public abstract class Node {
     protected String Name;
 
     public Node(String name) {
-        this.Name=name;
+        this.Name = name;
     }
-    public int getTime(){
+
+    public long getTime() {
         System.out.print("This is a Project, not a Task!");
         return 0;
     }
+
     public void addProject(String name) {
         System.out.print("This is a Task, not a Project!");
     }
+
     public void stopTask(String name) {
         System.out.print("This is a Project, not a Task!");
     }
+
     public void addTask(String name) {
         System.out.print("This is a Task, not a Project!");
     }
 
-    public Interval getCurrentInterval(){
+    public Interval getCurrentInterval() {
         System.out.print("This is a Project, not a Task!");
         return new Interval();
     }
 
-    public void print(){
+    public void print() {
         System.out.print("This is a Project, not a Task!");
     }
 
-    public String getName(){
+    public String getName() {
         return Name;
     }
-    public void showTree(int depth){
+
+    public void showTree(int depth) {
     }
-    
-    public void play(){
+
+    public void play() {
         System.out.print("This is a Project, not a Task!");
     }
 
-    public boolean isEqual(String name){
+    public boolean isEqual(String name) {
         return this.Name == name;
     }
-    
+
 }
--- src/Task.java
+++ src/Task.java
@@ -1,28 +1,50 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observer;
-public class Task {
-    private String Taskname;
+
+public class Task extends Node {
     private List<Interval> intervals;
 
-    public Task(String name){
+    public Task(String name) {
+        super(name);
         intervals = new ArrayList<Interval>();
-        Taskname = name;
     }
 
-    public void play(){
+    @Override
+
+    public long getTime() {
+        long aux = 0;
+        for (int i = 0; i < intervals.size(); i++) {
+            aux += intervals[i].getTime();
+        }
+    }
+
+    public void play() {
         intervals.add(new Interval());
     }
 
-    public Interval getCurrentInterval(){
-        return intervals.get(intervals.size()-1);
+    public Interval getCurrentInterval() {
+        return intervals.get(intervals.size() - 1);
     }
 
-    public void print(){
-        System.out.println("Tasca : " + Taskname + " ");
-        for (int i =0; i < intervals.size();i++){
-            System.out.println("Interval " + ( i + 1 ) + ": ");
+    public void print() {
+        System.out.println("Task : " + Name + " ");
+        for (int i = 0; i < intervals.size(); i++) {
+            System.out.println("Interval " + (i + 1) + ": ");
             intervals.get(i).printInterval();
         }
+    }
+
+    public void showTree(int depth) {
+        char aux = '+';
+        if (depth % 2 == 0) {
+            aux = '-';
+        }
+        for (int a = 0; a < depth; a++) {
+            System.out.print(aux);
+        }
+
+        System.out.print(Name + ", " + getTime() + "\n");
+
     }
 }
