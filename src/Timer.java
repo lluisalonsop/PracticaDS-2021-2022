405e744ba9ef413ae44d51508c21ef34e6a8a510
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2021-10-26 17:56:58 +0200

    Update Timer.java

--- src/Timer.java
+++ src/Timer.java
@@ -1,15 +1,29 @@
 import java.time.LocalDateTime;
 import java.util.Observable;
 import java.time.LocalTime;
-public class Timer extends Observable {
-    void tick() {
-        LocalDateTime CurrentDate = LocalDateTime.now();
-        setChanged();
-        notifyObservers(CurrentDate);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            System.out.println("Error Occurred.");
+public class Timer extends Observable implements Runnable{
+    private static Timer instance;
+    private Timer(){
+        Thread Timerthread = new Thread(this);
+        Timerthread.start();
+    }
+    @Override
+    public void run() {
+        while(true) {
+            LocalDateTime CurrentDate = LocalDateTime.now();
+            setChanged();
+            notifyObservers(CurrentDate);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                System.out.println("Error Occurred.");
+            }
         }
+    }
+    public static Timer getInstance(){
+        if (instance == null){
+            instance = new Timer();
+        }
+        return instance;
     }
 }
