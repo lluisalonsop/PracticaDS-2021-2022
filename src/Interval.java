4192993fce1a2310de27e09cd93e0059b2a9524d
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2021-10-26 17:56:11 +0200

    Update Interval.java

--- src/Interval.java
+++ src/Interval.java
@@ -10,19 +10,32 @@ import java.time.temporal.*;
 // To end the interval we simply have to un subscribe from the observer
 
 class Interval implements Observer{
-
+    private boolean Working;
     private LocalDateTime InitialDate;  //Revisar llibreria per format de les dates.
     private LocalDateTime EndDate;
     private long IntervalTime;
 
     public Interval(){
+        Working = true;
         InitialDate = LocalDateTime.now();
+        Timer.getInstance().addObserver(this);
     }
     public void update(Observable obj, Object arg){
         EndDate = (LocalDateTime) arg; // The end Date of the interval doesn't mean that it has finished yet.
         calculateTime();
     }
+    public void end(){
+        Working = false;
+        Timer.getInstance().deleteObserver(this);
+    }
 
+    public boolean getStatus(){
+        return Working;
+    }
+
+    public long getIntervalTime() {
+        return IntervalTime;
+    }
 
     public LocalDateTime getInitialDate(){
         return InitialDate;
@@ -36,7 +49,12 @@ class Interval implements Observer{
     }
     public void printInterval(){
         System.out.println("Initial date : " + InitialDate + " ");
-        System.out.println("End date :" + EndDate + " ");
+        if (Working == true){
+            System.out.println("Current date : " + EndDate + " ");
+        }
+        else{
+            System.out.println("End date : " + EndDate + " ");
+        }
         System.out.println("Total time : "+ IntervalTime + " \n");
     }
 }
