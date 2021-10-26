743da5009c3477e158ec0b7462b5df1a79773da0
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2021-10-26 17:56:30 +0200

    Update Task.java

--- src/Task.java
+++ src/Task.java
@@ -1,50 +1,37 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observer;
-
-public class Task extends Node {
+public class Task {
+    private String Taskname;
     private List<Interval> intervals;
 
-    public Task(String name) {
-        super(name);
+    public Task(String name){
         intervals = new ArrayList<Interval>();
+        Taskname = name;
     }
 
-    @Override
-
-    public long getTime() {
-        long aux = 0;
-        for (int i = 0; i < intervals.size(); i++) {
-            aux += intervals.get(i).calculateTime();
+    public void changeStatus(){
+        if (intervals.size() != 0) {
+            if (intervals.get(intervals.size() - 1).getStatus() == true ) {
+                intervals.get(intervals.size()-1).end();
+            }
+            else{
+                intervals.add(new Interval());
+            }
+        }
+        else {
+            intervals.add(new Interval());
         }
     }
 
-    public void play() {
-        intervals.add(new Interval());
-    }
-
-    public Interval getCurrentInterval() {
-        return intervals.get(intervals.size() - 1);
-    }
-
-    public void print() {
-        System.out.println("Task : " + Name + " ");
-        for (int i = 0; i < intervals.size(); i++) {
-            System.out.println("Interval " + (i + 1) + ": ");
+    public void print(){
+        System.out.println("\nPRINTING : " + Taskname + "\n");
+        long sumatory = 0;
+        for (int i =0; i < intervals.size();i++){
+            sumatory += intervals.get(i).getIntervalTime();
+            System.out.println("Interval " + ( i + 1 ) + ": ");
             intervals.get(i).printInterval();
         }
-    }
-
-    public void showTree(int depth) {
-        char aux = '+';
-        if (depth % 2 == 0) {
-            aux = '-';
-        }
-        for (int a = 0; a < depth; a++) {
-            System.out.print(aux);
-        }
-
-        System.out.print(Name + ", " + getTime() + "\n");
-
+        System.out.print("TOTAL TIME OF TASK " + Taskname + " in seconds: "+ sumatory +"\n");
     }
 }
