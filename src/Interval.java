89652a4c9d5fc7a5421dae732186dd5211c06de8
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2021-10-27 22:10:21 +0200

    Update Interval.java

--- src/Interval.java
+++ src/Interval.java
@@ -10,15 +10,11 @@ import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.temporal.*;
 
-/*
-Interval starts counting when initialized, adding it as an observer. 
-In every update EndDate is refreshed until the function end() is called,
-where it is deleted from teh observer list therefore not updating endDate anymore.  
-*/
+// To end the interval we simply have to un subscribe from the observer
 
 class Interval implements Observer {
     private boolean Working;
-    private LocalDateTime InitialDate; 
+    private LocalDateTime InitialDate; // Revisar llibreria per format de les dates.
     private LocalDateTime EndDate;
     private long IntervalTime;
 
@@ -36,7 +32,7 @@ class Interval implements Observer {
     }
 
     public void update(Observable obj, Object arg) {
-        EndDate = (LocalDateTime) arg; 
+        EndDate = (LocalDateTime) arg; // The end Date of the interval doesn't mean that it has finished yet.
         calculateTime();
     }
 
@@ -48,18 +44,22 @@ class Interval implements Observer {
     public boolean getStatus() {
         return Working;
     }
+
     public long getIntervalTime() {
         return IntervalTime;
     }
+
     public LocalDateTime getInitialDate() {
         return InitialDate;
     }
+
     public LocalDateTime getFinalDate() {
         return EndDate;
     }
 
     private void calculateTime() {
-        IntervalTime = InitialDate.until(EndDate, ChronoUnit.SECONDS); // Calculates the difference between the initial and end             
+        IntervalTime = InitialDate.until(EndDate, ChronoUnit.SECONDS); // Calculates the difference between the initial
+                                                                       // and en
     }
 
     public void printInterval() {
