bdcae3d7002d91e0de27403719ab4636a0a4084a
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2021-10-27 22:10:40 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,64 @@ import java.util.List;
 
 public class Main {
 
+    private static Node createFromJson(JSONObject jsonObject) {
+
+        if (jsonObject.get("class").toString().equals("Project")) {
+            Project result = new Project(jsonObject.getString("Name"));
+            JSONArray jsonArray = jsonObject.getJSONArray("activities");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                result.addNode(createFromJson(jsonArray.getJSONObject(i)));
+            }
+            return result;
+        }
+
+        if (jsonObject.get("class").toString().equals("Task")) {
+
+            List<Interval> intervals = new ArrayList<Interval>();
+
+            JSONArray jsonArray = jsonObject.getJSONArray("activities");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject aux = jsonArray.getJSONObject(i);
+                intervals.add(new Interval(aux.getBoolean("Working"), aux.getString("InitialDate"),
+                        aux.getString("EndDate")));
+            }
+
+            Task result = new Task(jsonObject.getString("Name"), intervals);
+
+            return result;
+
+        }
+
+        return new Project("");
+    }
+
+    private static Node fromJson(String route) {
+        try {
+            Reader fileReader = new FileReader(route);
+            char[] destination = new char[102400];
+            fileReader.read(destination, 0, destination.length);
+
+            String object = new String(destination);
+
+            JSONObject jsonobj = new JSONObject(object);
+            Node root = createFromJson(jsonobj);
+            fileReader.close();
+            return root;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Project("");
+    }
+
     public static void main(String[] args) {
 
         Timer.getInstance();
 
-        System.out.println("Test Starts");
 
         // ---Aqui deberia ir el import desde JSON-----
         Project Master = new Project("Master");
@@ -28,10 +81,9 @@ public class Main {
         Master.addNode(Lists);
         // -------------------------------------------
 
-        Json data = new Json("./Data/Initial.json");
-        Node root = data.fromJson(); // imported from JSON
-        data.saveJson(root.toJson()); //Save to JSON
-
+        Node root = fromJson("./Data/Initial.json"); // imported from JSON
+        new Printer(Master);
+        System.out.println("start test");
         System.out.println("Transportation Starts");
         transportation.changeStatus(); // START
 
@@ -103,18 +155,16 @@ public class Main {
         }
 
         System.out.println("Transportation Stops");
-        transportation.changeStatus(); // STOP
+        //transportation.changeStatus(); // STOP
 
         // ------PRINTS------
         // Deberia ser un print desde el root que es importado del JSON
-        transportation.print();
-        firstList.print();
-        secondList.print();
+        //transportation.print();
+        //firstList.print();
+        //secondList.print();
         // -------------------
 
         root.showTree(1); // no muestra el de la ejecuccion actual sino de la de guardadoo del JSON
-
-        System.out.println(Master.toJson());
     }
 
 }
