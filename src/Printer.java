e86686cc883d1760df39ecef4dc699227a2498de
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2021-11-23 03:32:58 +0100

    Printer with good checkstyle

--- src/Printer.java
+++ src/Printer.java
@@ -1,25 +1,19 @@
-import java.time.LocalDateTime;
-/*
-* Printer is a runnable class thet is in charge of printing information about the tree
-* */
-public class Printer implements Runnable{
-    private final Node rootPrj;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-    public Printer(Node root) {
-        System.out.println("                                        initial date                             final date                                  duration \n");
-        rootPrj = root;
-        Thread printThread = new Thread(this);
-        printThread.start();
-    }
-    @Override
-    public void run() {
-        while(true) {
-            rootPrj.printALLDOWN();
-            try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            System.out.println("Error Occurred.");
-        }
-    }
-    }
+public class Printer {
+  private final Node rootPrj;
+
+  public Printer(Node root) {
+    System.out.println("                                        initial date              "
+            + "               final date   " + "                               duration \n");
+    rootPrj = root;
+    schedule();
+  }
+
+  private void schedule() {
+    ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    executor.scheduleAtFixedRate(rootPrj::printAllDown, 0, 2000, TimeUnit.MILLISECONDS);
+  }
 }
