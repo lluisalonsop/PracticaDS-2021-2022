6f5bf22c70fcb85dc3110d55e9f7596c203f3c1f
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2021-11-23 03:31:28 +0100

    interval with checkstyle good

--- src/Interval.java
+++ src/Interval.java
@@ -1,15 +1,8 @@
-import java.time.LocalDate;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.Observable;
 import java.util.Observer;
-
-import org.json.*;
-
-import java.sql.Time;
-import java.time.LocalDateTime;
-import java.time.Period;
-import java.time.temporal.*;
-
+import org.json.JSONObject;
 /*
 Interval starts counting when initialized, adding it as an observer.
 In every update EndDate is refreshed until the function end() is called,
@@ -17,69 +10,61 @@ where it is deleted from teh observer list therefore not updating endDate anymor
 */
 
 class Interval implements Observer {
-    private boolean Working;
-    private LocalDateTime InitialDate;
-    private LocalDateTime EndDate;
-    private long IntervalTime;
+  private boolean working;
+  private final LocalDateTime initialDate;
+  private LocalDateTime endDate;
+  private long intervalTime;
 
-    public Interval() {
-        Working = true;
-        InitialDate = LocalDateTime.now();
-        Timer.getInstance().addObserver(this);
-    }
+  public Interval() {
+    working = true;
+    initialDate = LocalDateTime.now();
+    Timer.getInstance().addObserver(this);
+  }
 
-    public Interval(boolean Working, String InitialDate, String EndDate) {
-        this.Working = Working;
-        this.InitialDate = LocalDateTime.parse(InitialDate);
-        this.EndDate = LocalDateTime.parse(EndDate);
-        calculateTime();
-    }
+  public Interval(boolean workingToSet, String initialDateToSet, String endDateToSet) {
+    this.working = workingToSet;
+    this.initialDate = LocalDateTime.parse(initialDateToSet);
+    this.endDate = LocalDateTime.parse(endDateToSet);
+    calculateTime();
+  }
 
-    public void update(Observable obj, Object arg) {
-        EndDate = (LocalDateTime) arg;
-        calculateTime();
-    }
+  public void update(Observable obj, Object arg) {
+    endDate = (LocalDateTime) arg;
+    calculateTime();
+  }
 
-    public void end() {
-        Working = false;
-        Timer.getInstance().deleteObserver(this);
-    }
+  public void end() {
+    working = false;
+    Timer.getInstance().deleteObserver(this);
+  }
 
-    public boolean getStatus() {
-        return Working;
-    }
+  public boolean getStatus() {
+    return working;
+  }
 
-    public long getIntervalTime() {
-        return IntervalTime;
-    }
+  public long getIntervalTime() {
+    return intervalTime;
+  }
 
-    public LocalDateTime getInitialDate() {
-        return InitialDate;
-    }
+  public LocalDateTime getInitialDate() {
+    return initialDate;
+  }
 
-    public LocalDateTime getFinalDate() {
-        return EndDate;
-    }
+  public LocalDateTime getFinalDate() {
+    return endDate;
+  }
 
-    private void calculateTime() {
-        IntervalTime = InitialDate.until(EndDate, ChronoUnit.SECONDS); // Calculates the difference between the initial and end
-    }
+  private void calculateTime() {
+    intervalTime = initialDate.until(endDate, ChronoUnit.SECONDS);
+  }
 
-    public void printInterval() {
-        System.out.println("Initial date : " + InitialDate + " ");
-        if (Working == true) {
-            System.out.println("Current date : " + EndDate + " ");
-        } else {
-            System.out.println("End date : " + EndDate + " ");
-        }
-        System.out.println("Total time : " + IntervalTime + " \n");
-    }
 
-    public JSONObject toJson() {
-        JSONObject result = new JSONObject();
-        result.put("Working", Working);
-        result.put("InitialDate", InitialDate);
-        result.put("EndDate", EndDate);
-        return result;
-    }
+  //print interval deleted
+  public JSONObject toJson() {
+    JSONObject result = new JSONObject();
+    result.put("Working", working);
+    result.put("InitialDate", initialDate);
+    result.put("EndDate", endDate);
+    return result;
+  }
 }
