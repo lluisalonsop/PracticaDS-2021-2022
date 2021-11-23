d78a21a69e2e90f9b2351a7d7d87263542319346
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2021-11-23 03:35:42 +0100

    Timer with good checkstyle

--- src/Timer.java
+++ src/Timer.java
@@ -1,36 +1,30 @@
 import java.time.LocalDateTime;
 import java.util.Observable;
-import java.time.LocalTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-/*
-Timer is inizialized through getInstance() and is a singleton,
-when whis occurs the method run() is called in a thread and
-runs for the rest of the execution
-*/
-public class Timer extends Observable implements Runnable{
-    private static Timer instance;
-    private Timer(){
-        Thread Timerthread = new Thread(this);
-        Timerthread.start();
+public class Timer extends Observable  {
+  private static Timer instance;
+
+  private Timer() {
+    schedule();
+  }
+
+
+  private void schedule() {
+    ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    executor.scheduleAtFixedRate(() -> {
+      LocalDateTime currentDate = LocalDateTime.now();
+      setChanged();
+      notifyObservers(currentDate);
+    }, 0, 1, TimeUnit.MILLISECONDS);
+  }
+
+  public static Timer getInstance() {
+    if (instance == null) {
+      instance = new Timer();
     }
-    @Override
-    //This method will be executed by the Thread, simulating a clock
-    public void run() {
-        while(true) {
-            LocalDateTime CurrentDate = LocalDateTime.now();
-            setChanged();
-            notifyObservers(CurrentDate);
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                System.out.println("Error Occurred.");
-            }
-        }
-    }
-    public static Timer getInstance(){
-        if (instance == null){
-            instance = new Timer();
-        }
-        return instance;
-    }
+    return instance;
+  }
 }
