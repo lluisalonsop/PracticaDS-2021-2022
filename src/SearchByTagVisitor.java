f32049ad7d7ae2ea9c7ceb4c2ae56d68b0d4619b
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2021-11-23 03:34:31 +0100

    SearchByTag with good checkstyle

--- src/SearchByTagVisitor.java
+++ src/SearchByTagVisitor.java
@@ -1,22 +1,24 @@
+import java.util.Objects;
+
 public class SearchByTagVisitor implements Visitor {
-    private String tag;
+  private final String tag;
 
-    public SearchByTagVisitor(String tag) {
-        super();
-        this.tag = tag;
+  public SearchByTagVisitor(String tagToSet) {
+    super();
+    this.tag = tagToSet;
+  }
+
+  @Override
+  public Node visitNode(Node p) {
+    for (int i = 0; i < p.tags.size(); i++) {
+      if (Objects.equals(p.tags.get(i), tag)) {
+        return p;
+      }
     }
-
-    @Override
-
-    public Node visitNode(Node p) {
-        for (int i = 0; i < p.tags.size(); i++) {
-            if (p.tags.get(i) == tag) {
-                return p;
-            }
-        }
-        return new Project("null");
-    }
-
-    public void visitTask(Task t) {
-    }
+    return new Project("null");
+  }
 }
+/*  public void visitTask(Task t) {
+    }
+*/
+
