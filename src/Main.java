21cef85947121a0d388578ad65d37e64969d6f4c
lluisalonsop <57227302+lluisalonsop@users.noreply.github.com>
2021-11-24 21:58:30 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,11 @@
-import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.util.LinkedList;
 import java.util.Objects;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Main {
-  private static final Logger LOGGER_F1 = Logger.getLogger("LOGGER_F1");
-  private static final Logger LOGGER_F2 = Logger.getLogger("LOGGER_F2");
-
-  private static void initHandlers_F1() {
-    try {
-      Handler fileHandler = new FileHandler("./out/production/timetracker/logback_F1.xml", false);
-      fileHandler.setLevel(Level.ALL);
-      Handler consoleHandler = new ConsoleHandler();
-      consoleHandler.setLevel(Level.ALL);
-      LOGGER_F1.addHandler(fileHandler);
-      LOGGER_F1.addHandler(consoleHandler);
-      LOGGER_F1.log(Level.INFO, "Logger initialized");
-
-    } catch (IOException e) {
-      LOGGER_F1.log(Level.SEVERE, "Error initializing Logger");
-    }
-  }
-
-  private static void initHandlers_F2() {
-    try {
-      Handler fileHandler = new FileHandler("./out/production/timetracker/logback_F2.xml", false);
-      fileHandler.setLevel(Level.ALL);
-      Handler consoleHandler = new ConsoleHandler();
-      consoleHandler.setLevel(Level.ALL);
-      LOGGER_F2.addHandler(fileHandler);
-      LOGGER_F2.addHandler(consoleHandler);
-      LOGGER_F2.log(Level.INFO, "Logger initialized");
-
-    } catch (IOException e) {
-      LOGGER_F2.log(Level.SEVERE, "Error initializing Logger");
-    }
-  }
-
+  private static  Logger logger = null;
   public static void main(String[] args) {
-    initHandlers_F1();
-    initHandlers_F2();
+    logger = LoggerFactory.getLogger(Main.class);
     Timer.getInstance();
     // Cambiar system.outs a logger.log
     // ---Aqui deberia ir el import desde JSON-----
@@ -69,79 +31,79 @@ public class Main {
     new PrinterVisitor(master);
 
     if (Objects.equals(result.getLast().name, secondList.name)) {
-      LOGGER_F2.log(Level.INFO, "Search by tag is working!");
+      logger.info("Search_by_tag_is_working!");
     } else {
-      LOGGER_F2.log(Level.WARNING, "Search by tag isn't working!");
+      logger.warn("Search_by_tag_isn't_working!");
     }
 
     Json data = new Json("./Data/Initial.json");
     final Node root = data.fromJson(); // imported from JSON
 
-    LOGGER_F1.log(Level.INFO, "Start Test");
-    LOGGER_F1.log(Level.INFO, "Transportation Starts");
+    logger.info("Start_Test");
+    logger.info("Transportation_Starts");
     transportation.changeStatus(); // START
     // 4 Second Sleep
     try {
       Thread.sleep(4000);
     } catch (InterruptedException e) {
-      LOGGER_F1.log(Level.SEVERE, "4 second sleep suffered an interruption");
+      logger.warn("4_second_sleep_suffered_an_interruption");
     }
 
-    LOGGER_F1.log(Level.INFO, "Transportation Stops");
+    logger.info("Transportation_Stops");
     transportation.changeStatus(); // STOP
     // 2 Second Sleep
     try {
       Thread.sleep(2000);
     } catch (InterruptedException e) {
-      LOGGER_F1.log(Level.SEVERE, "2 second sleep suffered an interruption");
+      logger.warn("2_second_sleep_suffered_an_interruption");
     }
 
-    LOGGER_F1.log(Level.INFO, "First List Start");
+    logger.info("First_List_Start");
     firstList.changeStatus(); // START
     // 6 Second Sleep
     try {
       Thread.sleep(6000);
     } catch (InterruptedException e) {
-      LOGGER_F1.log(Level.SEVERE, "6 second sleep suffered an interruption");
+      logger.warn("6_second_sleep_suffered_an_interruption");
     }
 
-    LOGGER_F1.log(Level.INFO, "Second List Starts");
+    logger.info("Second_List_Starts");
     secondList.changeStatus(); // START
     // 4 Second Sleep
     try {
       Thread.sleep(4000);
     } catch (InterruptedException e) {
-      LOGGER_F1.log(Level.SEVERE, "4 second sleep suffered an interruption");
+      logger.warn("4_second_sleep_suffered_an_interruption");
     }
 
-    LOGGER_F1.log(Level.INFO, "First List Stops");
+    logger.info("First_List_Stops");
     firstList.changeStatus(); // STOP
     // 2 Second Sleep
     try {
       Thread.sleep(2000);
     } catch (InterruptedException e) {
-      LOGGER_F1.log(Level.SEVERE, "2 second sleep suffered an interruption");
+      logger.info("2_second_sleep_suffered_an_interruption");
     }
-    LOGGER_F1.log(Level.INFO, "Second list Stops");
+    logger.info("Second_list_Stops");
     secondList.changeStatus(); // STOP
     // 2 Second Sleep
     try {
       Thread.sleep(2000);
     } catch (InterruptedException e) {
-      LOGGER_F1.log(Level.SEVERE, "2 second sleep suffered an interruption");
+      logger.warn("2_second_sleep_suffered_an_interruption");
     }
 
-    LOGGER_F1.log(Level.INFO, "Transportation Starts");
+    logger.info("Transportation_Starts");
     transportation.changeStatus(); // START
 
     // 4 Second Sleep
     try {
       Thread.sleep(4000);
     } catch (InterruptedException e) {
-      LOGGER_F1.log(Level.SEVERE, "4 second sleep suffered an interruption");
+      logger.warn("4_second_sleep_suffered_an_interruption");
     }
 
-    LOGGER_F1.log(Level.INFO, "Transportation Stops");
+    logger.info("Transportation Stops");
 
     data.saveJson(root.toJson()); // Save to JSON
 
